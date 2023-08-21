e9032f261e3e9360d00f23d06c3dbb8243641221
Waruna <warunahello@gmail.com>
2023-08-21 23:51:54 +0530

    added front to the hr dashboard

--- Contoso-HR-System/resources/views/hr/index.blade.php
+++ Contoso-HR-System/resources/views/hr/index.blade.php
@@ -1,11 +1,13 @@
-<x-employee-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+<!-- resources/views/specific-view.blade.php -->
+
+@include('layouts.hr') <!-- Include the hr layout -->
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                {{ __("You're logged in!") }}
             </div>
         </div>
     </div>
-</x-employee-layout>
+</div>
